36ebcd27d9bedf5a33df7f8d7e0d3604b62b47f2
Shubham Darade <111228660+daradeshubham72@users.noreply.github.com>
2022-08-30 02:01:55 +0530

    Add files via upload

---
+++ HelloWorld/src/FinalDemo.java
@@ -0,0 +1,40 @@
+package com.cg.accessmodifiers;
+//class methods and variables
+public class FinalDemo {
+
+	public static void main(String[] args) {
+	
+	final int age=25; //constant
+	//later after putting final before int i cannot change age .
+	
+	
+	
+	}
+		final class Parent
+		{
+      void property()
+      {
+        System.out.println("house wealth plots jewelery car");
+        }
+      //parent class method
+      final void marry()
+      { 
+    	  System.out.println("Srilatha");
+    	  
+      }
+		}
+		class Shubham extends Parent //inheritance
+		{		//override
+		
+	void marry()
+	{
+		System.out.println("Katrina");
+	
+	}
+		}
+}
+
+
+
+
+
